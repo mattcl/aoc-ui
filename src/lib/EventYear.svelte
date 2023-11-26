896a4d7eefe384c4d246f9392184056a6ee33c4e
Matt Chun-Lum <matt@questionable.engineering>
2023-11-26 01:27:57 -0800

    feat: allow filtering out participants

--- src/lib/EventYear.svelte
+++ src/lib/EventYear.svelte
@@ -4,6 +4,8 @@
   import CumulativeGraph from './CumulativeGraph.svelte'
   import PercentRuntimeGraph from './PercentRuntimeGraph.svelte'
   import PerDayGraph from './PerDayGraph.svelte'
+  import Checkbox from '@smui/checkbox'
+  import FormField from '@smui/form-field';
 
   let base_url = import.meta.env.VITE_API_BASE;
 
@@ -12,17 +14,45 @@
   let summaries: Summary[] = [];
   let participants: Participant[] = [];
 
+  $: filters = participants.map((x) => {
+    return { name: x.name, disabled: false };
+  });
+  let selected = [];
+
   fetch(`${base_url}/api/v1/summaries?year=${year}`)
     .then((response) => response.json())
     .then((json) => (summaries = json));
 
   fetch(`${base_url}/api/v1/participants?year=${year}`)
     .then((response) => response.json())
-    .then((json) => (participants = json));
+    .then((json) => (participants = json))
+    .then(() => {
+      // this is a hack to exclude some very large runtimes from showing by
+      // default for 2022 only
+      selected = participants.filter((x) => year > 2022 || x.name != 'pting').map((x) => x.name);
+    });
+
+  // I'm using this function to hide the summaries dependency from the reactive
+  // filteredSummaries, since I don't want changes to that triggering this.
+  function getFiltered(sel: string[]) {
+    return summaries.filter((item) => sel.includes(item.participant));
+  }
+
+  $: filteredSummaries = getFiltered(selected);
 
   let pipeline = `http://ci.papercode.net:8080/teams/main/pipelines/aoc${year}`
 
 </script>
+
+<h3>Enabled participants</h3>
+<div>
+  {#each filters as option}
+    <FormField>
+      <Checkbox bind:group={selected} value={option.name} disabled={option.disabled} />
+      <span slot="label">{option.name}</span>
+    </FormField>
+  {/each}
+</div>
 
 {#if year == 2022}
   <p>2022 is incomplete because it was run before the spec and new benchmarking,
@@ -32,7 +62,7 @@
   <br />
   <a href="{ pipeline }" target="_blank">CI Pipeline</a>
 {/if}
-<Summaries year={year} summaries={summaries} participants={participants} />
-<PerDayGraph items={summaries} />
-<CumulativeGraph items={summaries} />
-<PercentRuntimeGraph items={summaries} />
+<Summaries year={year} summaries={filteredSummaries} participants={participants} />
+<PerDayGraph items={filteredSummaries} />
+<CumulativeGraph items={filteredSummaries} />
+<PercentRuntimeGraph items={filteredSummaries} />
