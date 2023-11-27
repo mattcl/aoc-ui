07b57abd3f9b7610c03f80215cc53fb6bc9ffe64
Matt Chun-Lum <matt@questionable.engineering>
2023-11-26 17:29:19 -0800

    feat: add log scale toggle for the per-day graph

--- src/lib/PerDayGraph.svelte
+++ src/lib/PerDayGraph.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
+  import type { Summary } from './common';
   import Plot from 'svelte-plotly.js';
-  import type { Summary } from './common'
+  import Radio from '@smui/radio';
+  import FormField from '@smui/form-field';
 
   export let items: Summary[] = [];
   export let days: number[] = [];
+
+  let defaultColors = ["#5ebf72", "#3f6f94", "#70b1ea", "#9d42b7", "#ea85f5", "#805d94", "#c39dd9", "#2d70f0", "#1abdc5", "#41763f", "#f85582", "#f6932e"];
+
+
+  let scaleOptions = ['linear', 'log'];
+  let scale = 'linear';
 
   $: data = makeData(items, days);
 
@@ -35,19 +43,27 @@
   }
 </script>
 
-
-{#if data.length == 0 }
-
-{:else}
+{#if data.length > 0}
   <h2>Runtime by Day</h2>
+  <div>
+    <span>Scale:</span>
+    {#each scaleOptions as option }
+      <FormField>
+        <Radio bind:group={scale} value={option} />
+        <span slot="label">{option}</span>
+      </FormField>
+    {/each}
+  </div>
   <Plot
     {data}
     layout={{
+      colorway: defaultColors,
       height: 500,
       yaxis: {
         title: {
           text: "Time (ms)"
-        }
+        },
+        type: scale
       },
       xaxis: {
         title: {
